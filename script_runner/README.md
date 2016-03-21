12ea9ba2c0f8258e8aeb9ce46146e07fb659c259
Piotr Król <piotr.krol@3mdeb.com>
2016-03-21 16:39:56 +0100

    script_runner: pass script encoded with base64 inside JSON between 2 functions

---
+++ script_runner/README.md
@@ -0,0 +1,2 @@
+Script runner show how arbitrary file (ie. script) can be passed in JSON between two
+functions and executed. Base64 and JSON maybe useful when passing over network.
--- script_runner/script_runner.py
+++ script_runner/script_runner.py
@@ -1,15 +1,35 @@
 #!//usr/bin/env python
 
 import json
+import subprocess
 from base64 import b64encode, b64decode
 
-def decode(b64str):
-    pass
+def decode(json_obj):
+    j = json.loads(json_obj)
+    script = b64decode(j['script'])
+    with open("script-1.sh", 'w') as f:
+        f.write(script)
+    run("script-1.sh")
 
 def run(script):
-    pass
+    proc = subprocess.Popen(["chmod", "+x", script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if out:
+        print out
+    if err:
+        print err
+    proc = subprocess.Popen(["./{0}".format(script)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if out:
+        print out
+    if err:
+        print err
+    print "retruncode: {0}".format(proc.returncode)
 
 if __name__ == "__main__":
     with open("script.sh", 'r') as f:
         script = f.read()
-        decode(b64encode(script))
+        data = {
+                'script': b64encode(script)
+                }
+        decode(json.dumps(data))
